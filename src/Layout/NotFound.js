6c7857b32e8a765fceab35fb0d6aabcf18f5b469
Valerie Gutierrez <Valeriengutierrez@yahoo.com>
2021-07-30 09:17:06 -0500

    initial commit

---
+++ src/Layout/NotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NotFound() {
+  
+  //need to loop
+   
+  //if(!{url}){} or something like that =====Display a "Not found" message if the user visits a URL that does not exist.
+  return (
+    <div className="NotFound">
+      <h1>Not Found</h1>
+    </div>
+  );
+}
+
+export default NotFound;
