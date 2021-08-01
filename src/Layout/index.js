d4863ef265fdbeb57a258c368b7da2050414bb7e
Valerie Gutierrez <Valeriengutierrez@yahoo.com>
2021-08-01 15:58:19 -0500

    home buttons

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
+import Home from "./Home"
+import {Route, Switch} from 'react-router-dom'
+
 
 function Layout() {
   return (
     <div>
       <Header />
       <div className="container">
+        <Switch>
+      <Route exact path='/'>
+        <Home />
+        </Route>
         {/* TODO: Implement the screen starting here */}
+        {/*add routes and paths for views*/}
+        {/*at / shows a list of decks and 4 buttons*/}
         <NotFound />
+        </Switch>
       </div>
     </div>
   );
